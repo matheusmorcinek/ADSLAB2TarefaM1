d0907d5ffc3a106b3ad275da8af0a471a98717f1
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-03-06 15:48:54 -0300

    tasks completed

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,14 +1,86 @@
 package ADSLAB2TarefaM1;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+
 /**
  *
  * 
-    [ ] No construtor da classe, receba por parâmetro um valor inteiro referente às dimensões do tabuleiro. Por exemplo, se o inteiro recebido for 3, o tabuleiro terá dimensões 3x3; caso o valor recebido seja 7, o tabuleiro terá dimensões 7x7, e assim sucessivamente. Com este valor recebido, inicialize o tabuleiro.
-    [ ] Crie o método realizaJogada, que recebe dois inteiros (referentes a uma linha e uma coluna do tabuleiro) e um caractere, referente a jogada a ser realizada ('X' ou 'O'). Este método retorna verdadeiro ou falso, e simplesmente coloca no tabuleiro a jogada realizada. Caso já tenha uma jogada na posição indicada, o método retorna falso, indicando que a jogada não pode ser realizada. Caso a jogada seja realizada corretamente, o método retorna verdadeiro.
-    [ ] Crie um método chamado verificaGanhador, que retorna um valor booleano. Este método analisa o tabuleiro e retorna verdadeiro caso o jogo tenha um vencedor, e falso caso contrário. O jogo tem um vencedor caso as duas diagonais (principal e secundária) estejam preenchidas com o mesmo caractere ('X' ou 'O'), ou quando alguma linha estiver preenchida com o mesmo caractere ('X' ou 'O'), ou ainda quando alguma coluna estiver preenchida com o mesmo caractere ('X' ou 'O').
-    [ ] Na classe JogoDaVelha, faça o método toString, que retorna uma String que refere-se ao estado atual do tabuleiro. 
+    [x] No construtor da classe, receba por parâmetro um valor inteiro referente às dimensões do tabuleiro.
+     Por exemplo, se o inteiro recebido for 3, o tabuleiro terá dimensões 3x3; caso o valor recebido seja 7, o tabuleiro terá dimensões 7x7, e assim sucessivamente. 
+     Com este valor recebido, inicialize o tabuleiro.
+    [x] Crie o método realizaJogada, que recebe dois inteiros (referentes a uma linha e uma coluna do tabuleiro) e um caractere, referente a jogada a ser realizada ('X' ou 'O').
+         Este método retorna verdadeiro ou falso, e simplesmente coloca no tabuleiro a jogada realizada. Caso já tenha uma jogada na posição indicada,
+      o método retorna falso, indicando que a jogada não pode ser realizada. Caso a jogada seja realizada corretamente, o método retorna verdadeiro.
+    [ ] Crie um método chamado verificaGanhador, que retorna um valor booleano. Este método analisa o tabuleiro e retorna verdadeiro caso o jogo tenha um vencedor,
+     e falso caso contrário. O jogo tem um vencedor caso as duas diagonais (principal e secundária) estejam preenchidas com o mesmo caractere ('X' ou 'O'),
+     ou quando alguma linha estiver preenchida com o mesmo caractere ('X' ou 'O'), ou ainda quando alguma coluna estiver preenchida com o mesmo caractere ('X' ou 'O').
+    [x] Na classe JogoDaVelha, faça o método toString, que retorna uma String que refere-se ao estado atual do tabuleiro. 
  * 
  */
 public class JogoDaVelha {
 
+    private int dimensao;
+    private char[][] tabuleiro;
+
+    public JogoDaVelha(int dimensao) {
+
+        this.dimensao = dimensao;
+        tabuleiro = new char[dimensao][dimensao];
+        // tabuleiro = new char[][]{{'X','O','O'},{'O','X','O'},{'O','O','X'}};
+        
+    }
+   
+    public int getDimensao() {
+        return dimensao;
+    }
+
+    public char[][] getTabuleiro() {
+        return tabuleiro;
+    }
+
+    public boolean realizaJogada(int x, int y, char valor){
+      
+        if (valor != 'X' || valor != 'O' || valor != 'x' || valor != 'o') {
+
+            return false;
+        }
+
+        if (tabuleiro[x][y] == 0) {
+
+            tabuleiro[x][y] = valor;
+            return true;
+        } 
+
+        return false;
+    }
+
+
+    //TODO
+    public boolean verificaGanhador() {
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+
+        System.out.println("=============================");
+        System.out.println();
+
+        for (int x = 0; x < tabuleiro.length; x++) {
+            for (int y = 0; y < tabuleiro.length; y++) {
+                
+                System.out.print(MessageFormat.format("| {0} ", tabuleiro[x][y]));
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("=============================");
+
+        return "JogoDaVelha [dimensao=" + dimensao + ", tabuleiro=" + Arrays.toString(tabuleiro) + "]";
+    }
+
+    
 }
--- Principal.java
+++ Principal.java
@@ -1,23 +1,51 @@
 package ADSLAB2TarefaM1;
 
-
 /**
  * 
-    [ ] Crie dois jogadores, com nomes solicitados pelo teclado
-    [ ] Crie um objeto do tipo JogoDaVelha com um tamanho de tabuleiro informado pelo usuário via teclado  
-    [ ] Desenvolva, utilizando os métodos criados nas classes JogoDaVelha e Jogador, a lógica do jogo da velha. Os jogadores devem indicar qual posição (linha e coluna) querem jogar. O primeiro jogador joga com o 'X', enquanto o segundo com a 'O'  
-    [ ] Ao final de cada jogo, você deve aumentar a pontuação do jogador vencedor e perguntar se eles desejam jogar novamente. Quando os jogadores não quiserem mais jogar, imprima na tela as informações do jogador vencedor
+ * [ ] Crie dois jogadores, com nomes solicitados pelo teclado 
+ * [ ] Crie um
+ * objeto do tipo JogoDaVelha com um tamanho de tabuleiro informado pelo usuário
+ * via teclado 
+ * [ ] Desenvolva, utilizando os métodos criados nas classes
+ * JogoDaVelha e Jogador, a lógica do jogo da velha. Os jogadores devem indicar
+ * qual posição (linha e coluna) querem jogar. O primeiro jogador joga com o
+ * 'X', enquanto o segundo com a 'O' 
+ * [ ] Ao final de cada jogo, você deve
+ * aumentar a pontuação do jogador vencedor e perguntar se eles desejam jogar
+ * novamente. Quando os jogadores não quiserem mais jogar, imprima na tela as
+ * informações do jogador vencedor
  */
 
 public class Principal {
 
-    // int[][] arr = new int[10][20]; 
-    // arr[0][0] = 1; 
+    // int[][] arr = new int[10][20];
+    // arr[0][0] = 1;
 
     public static void main(String[] args) {
-        
-        
+
+        JogoDaVelha jv = new JogoDaVelha(3);
+        jv.toString();
+
+        // char[][] arr = new char[][]{{'a','b','c'},{'d','e','f'},{'g','h','i'}};
+
+        // for (int x = 0; x < arr.length; x++) {
+        //     for (int y = 0; y < arr.length; y++) {
+                
+        //         System.out.print(arr[x][y]);
+        //     }
+        //     System.out.println();
+        // }
+
+       
+        // char value = 'M'; 
+
+        // if (value == 0) {
+        //     System.out.println("vazio");
+        // } else {
+        //     System.out.println(value);
+        // }
+
+       
     }
 
-   
 }
