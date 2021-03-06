84fb1b75ee3cf987bfbaa240088c0ac9c28706d0
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-03-06 15:07:04 -0300

    jogador, feito

--- Jogador.java
+++ Jogador.java
@@ -2,17 +2,40 @@ package ADSLAB2TarefaM1;
 
 /**
  * 
-   [ ] No construtor de jogador, receba parâmetros para inicializar o nome dos atributos,
+   [x] No construtor de jogador, receba parâmetros para inicializar o nome dos atributos,
        e inicialize os pontos em zero
-   [ ] Crie os métodos de acesso  
-   [ ] Crie o toString 
+   [x] Crie os métodos de acesso  
+   [x] Crie o toString 
  * 
  */
 
 public class Jogador {
- 
+     
+    private String nome;
+    private int pontuacao = 0;
 
- 
-    
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador [nome=" + nome + ", pontuacao=" + pontuacao + "]";
+    }
 }
