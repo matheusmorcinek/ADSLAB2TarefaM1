8761092445de24bd34cf02d881fbb78c0d0e3870
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-03-06 14:57:49 -0300

    tasks

--- Jogador.java
+++ Jogador.java
@@ -1,5 +1,18 @@
 package ADSLAB2TarefaM1;
 
+/**
+ * 
+   [ ] No construtor de jogador, receba parâmetros para inicializar o nome dos atributos,
+       e inicialize os pontos em zero
+   [ ] Crie os métodos de acesso  
+   [ ] Crie o toString 
+ * 
+ */
+
 public class Jogador {
+ 
+
+ 
     
+
 }
